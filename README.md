89cee84f574a3a3360cb48fc99be7544f90a121a
Arnaud Roger <arnaud.roger@gmail.com>
2019-08-20 11:52:15 +0100

    init setup

---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/jOOQ/jOOQ/tree/master/jOOQ-examples/Sakila
---
+++ src/main/kotlin/com/github/arnaudroger/bjug201908220/Bjug201908220Application.kt
@@ -0,0 +1,11 @@
+package com.github.arnaudroger.bjug201908220
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Bjug201908220Application
+
+fun main(args: Array<String>) {
+	runApplication<Bjug201908220Application>(*args)
+}
